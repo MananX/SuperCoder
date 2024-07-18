636af2cee708d23aee2962f97ac4e7a564bc1bb6
MananX <Singhmanandeep81@gmail.com>
2024-07-18 18:59:32 +0530

    name fixes

--- app/types/response/fetch_organisation_user_response.go
+++ app/types/response/fetch_organisation_user_response.go
@@ -1,12 +1,12 @@
 package response
 
 type FetchOrganisationUserResponse struct {
-	Success bool            `json:"success"`
-	Error   interface{}     `json:"error,omitempty"`
-	Users   []*UserResponse `json:"users,omitempty"`
+	Success bool             `json:"success"`
+	Error   interface{}      `json:"error,omitempty"`
+	Users   []*UsersResponse `json:"users,omitempty"`
 }
 
-type UserResponse struct {
+type UsersResponse struct {
 	ID             uint   `json:"id"`
 	Name           string `json:"name"`
 	Email          string `json:"email"`
